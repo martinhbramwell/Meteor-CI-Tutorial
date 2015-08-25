806d3ca00a0dddbabac9c605d11ad431463706f7
mhb.warehouseman@gmail.com <Warehouseman>
2015-08-25 19:53:38 -0400

    Add status check  Changes committed: 	modified:   ../gitHooksUseCommitMsg.sh

--- gitHooksUseCommitMsg.sh
+++ gitHooksUseCommitMsg.sh
@@ -4,17 +4,17 @@ echo "Stashed"
 git checkout gh-pages
 echo "On branch gh-pages"
 git checkout master -- index.html
-echo "Pulled inde.html"
+echo "Pulled index.html"
 git checkout master -- Prep4MeteorCI_A/index.html
 git checkout master -- Prep4MeteorCI_A/concatenatedSlides.MD
 git checkout master -- Prep4MeteorCI_B/index.html
 git checkout master -- Prep4MeteorCI_B/concatenatedSlides.MD
 echo "Pulled all"
 git commit -am $(cat $1)
-echo "Pommitted"
+echo "Committed"
 git push
 echo "Pushed"
 git checkout master
-echo "On branch master"
+echo "- - - back on branch master - - -"
 git stash apply
 echo "Reverted stash"
