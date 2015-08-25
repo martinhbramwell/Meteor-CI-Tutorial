b075fc595447950193667821b91600d328c63842
mhb.warehouseman@gmail.com <Warehouseman>
2015-08-25 19:41:57 -0400

    Doco

--- gitHooksUseCommitMsg.sh
+++ gitHooksUseCommitMsg.sh
@@ -1,12 +1,20 @@
 echo "Prepared message : "
 git stash
+echo "Stashed"
 git checkout gh-pages
+echo "On branch gh-pages"
 git checkout master -- index.html
+echo "Pulled inde.html"
 git checkout master -- Prep4MeteorCI_A/index.html
 git checkout master -- Prep4MeteorCI_A/concatenatedSlides.MD
 git checkout master -- Prep4MeteorCI_B/index.html
 git checkout master -- Prep4MeteorCI_B/concatenatedSlides.MD
+echo "Pulled all"
 git commit -am $(cat $1)
+echo "Pommitted"
 git push
+echo "Pushed"
 git checkout master
+echo "On branch master"
 git stash apply
+echo "Reverted stash"
