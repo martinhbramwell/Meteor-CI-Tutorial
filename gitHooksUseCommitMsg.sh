7c0eafcaef140328e4f296a8d343b2eb4c7ea1ef
mhb.warehouseman@gmail.com <Warehouseman>
2015-08-25 19:38:48 -0400

    test hook

--- gitHooksUseCommitMsg.sh
+++ gitHooksUseCommitMsg.sh
@@ -1,8 +1,4 @@
 echo "Prepared message : "
-cat $1
-MSG=$(cat $1)
-echo ${MSG}
-exit
 git stash
 git checkout gh-pages
 git checkout master -- index.html
@@ -10,7 +6,7 @@ git checkout master -- Prep4MeteorCI_A/index.html
 git checkout master -- Prep4MeteorCI_A/concatenatedSlides.MD
 git checkout master -- Prep4MeteorCI_B/index.html
 git checkout master -- Prep4MeteorCI_B/concatenatedSlides.MD
-git commit -am $($1)
+git commit -am $(cat $1)
 git push
 git checkout master
 git stash apply
