506d056123913380ffeac4bb221d66a9d1703aca
mhb.warehouseman@gmail.com <Warehouseman>
2015-08-26 15:05:30 -0400

    Use styles

--- gitHooksUseCommitMsg.sh
+++
@@ -1,8 +0,0 @@
-echo "Committing gh-pages"
-#	echo "Preparing message ##################################################"
-#	git checkout gh-pages 
-#	git checkout master -- index.html
-#	git commit -am 
-#	git push
-#	git push origin gh-pages
-
