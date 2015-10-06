c01827fe91417ab491f7fb5428296d37ff9dfef8
mhb.warehouseman@gmail.com <Warehouseman>
2015-10-06 08:47:34 -0400

    patch PKG_NAME in logger.js after wget  Changes committed: 	modified:   Part07_ProductionLogging.sh

--- Part07_ProductionLogging.sh
+++ Part07_ProductionLogging.sh
@@ -47,6 +47,7 @@ if [ "${RUN_RULE}" != "n" ]; then
   pushd ~/${PARENT_DIR}/${PROJECT_NAME}/packages/${PKG_NAME} >/dev/null;
 
   wget -O logger.js https://raw.githubusercontent.com/martinhbramwell/Meteor-CI-Tutorial/master/fragments/logger.js
+  sed -i -e "s/\${PKG_NAME}/${PKG_NAME}/"
 
   popd >/dev/null;
 
