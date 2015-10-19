d1436594ca9a8a7f3663196cb3992a72b96295b7
Martin H. Bramwell <martinhbramwell@gmail.com>
2015-10-19 18:15:57 -0400

    Update yourpackage.js

--- fragments/yourpackage.js
+++ fragments/yourpackage.js
@@ -2,6 +2,7 @@ const Client = Npm.require('swagger-client');
 const swaggerSpecURL = 'http://petstore.swagger.io/v2/swagger.json';
 const swagger = new Client({
   url: swaggerSpecURL,
+  success: function getPet() {
     swagger.pet.getPetById(
       { petId: 6133627028 }, {responseContentType: 'application/json'},
       function log(pet) { Logger.info('(Async) Pet #' + pet.obj.id, ' -- ' + pet.obj.name);  }
