2a043fb0e568b7d5d666a936dd600855a85a3b61
mhb.warehouseman@gmail.com <Warehouseman>
2015-10-20 06:35:28 -0400

    fix loop

--- fragments/yourpackage.js
+++ fragments/yourpackage.js
@@ -1,13 +1,13 @@
 const Client = Npm.require('swagger-client');
 const swaggerSpecURL = 'http://petstore.swagger.io/v2/swagger.json';
-Logger.debug('mypackage :: ', 'starting');
+Logger.debug('Swagger package :: ', 'starting');
 
 const TestPet = 6133627028;
 const swagger = new Client({
   url: swaggerSpecURL,
   success: function getPet() {
-  	Logger.debug('(Async) Looking for Pet #', TestPet);
-    for (idx = TestPet; idx <= TestPet + 3; idx++) {
+    for (idx = TestPet; idx < TestPet + 4; idx++) {
+      Logger.debug('(Async) Looking for Pet #', idx);
       swagger.pet.getPetById(
         { petId: idx}, {responseContentType: 'application/json'},
         function log(pet) { Logger.info('(Async) Pet #' + pet.obj.id, ' -- ' + pet.obj.name);  }
